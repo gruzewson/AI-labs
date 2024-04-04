490359bb472549abf0727166be17c9ed7a0b2db2
gruzewson <gruzewskimarcel@gmail.com>
2024-04-04 21:05:13 +0200

    final version??

--- lab3/ava.py
+++ lab3/ava.py
@@ -1,13 +1,12 @@
 from exceptions import GameplayException
 from connect4 import Connect4
-#from randomagent import RandomAgent
+from randomagent import RandomAgent
 from minmaxagent import MinMaxAgent
 from alphabetaagent import AlphaBetaAgent
 
 connect4 = Connect4(width=7, height=6)
-agent1 = MinMaxAgent('o')
-agent2 = AlphaBetaAgent('x')
-#agent2 = MinMaxAgent('x')
+agent1 = AlphaBetaAgent('o')
+agent2 = RandomAgent('x')
 while not connect4.game_over:
     connect4.draw()
     try:
